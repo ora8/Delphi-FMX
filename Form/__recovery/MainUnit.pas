2d85eadb1f557efd60b8199de23fc883692ba309
Urs <ora8@gmx.ch>
2025-10-03 18:52:13 +0200

    Add Form

---
+++ Form/__recovery/MainUnit.pas
@@ -0,0 +1,46 @@
+unit MainUnit;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs,
+  FMX.Controls.Presentation, FMX.StdCtrls;
+
+type
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Button2: TButton;
+    Button3: TButton;
+    procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+    procedure Button3Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.fmx}
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  Fill.Color := TAlphaColors.Blue;
+end;
+
+procedure TForm1.Button2Click(Sender: TObject);
+begin
+  Fill.Color := TAlphaColors.Black
+end;
+
+procedure TForm1.Button3Click(Sender: TObject);
+begin
+  Fill.Color := TAlphaColors.Red;
+end;
+
+end.
