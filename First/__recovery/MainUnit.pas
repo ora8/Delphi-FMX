586ebc1a8d5d0e0b8d3e99995d285746d14818be
Urs <ora8@gmx.ch>
2025-10-03 18:12:33 +0200

    First sample app

---
+++ First/__recovery/MainUnit.pas
@@ -0,0 +1,35 @@
+unit MainUnit;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs,
+  FMX.Controls.Presentation, FMX.StdCtrls;
+
+type
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Label1: TLabel;
+    procedure Button1Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.fmx}
+{$R *.Macintosh.fmx MACOS}
+{$R *.Windows.fmx MSWINDOWS}
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  Label1.Text := 'Hello World!';
+end;
+
+end.
