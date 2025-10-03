f2b4518929e1fa44e35d3f6b7717231394b574b3
Urs <ora8@gmx.ch>
2025-10-03 17:13:40 +0200

    init

---
+++ First/First.dpr
@@ -0,0 +1,14 @@
+program First;
+
+uses
+  System.StartUpCopy,
+  FMX.Forms,
+  MainUnit in 'MainUnit.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
